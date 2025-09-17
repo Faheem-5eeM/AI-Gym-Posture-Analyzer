13a5ab17b0b9fbf8e972e05639c78b99a5e18f0f
Irfan <xl.irfan124@gmail.com>
2025-09-17 07:10:07 +0530

    Added Images

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,29 +2,38 @@ import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <div className="">
+    <div className="h-screen">
       // Header Section
       <div className=" ">
-        <h1 className="text-white text-5xl">WELCOME TO FORM AI</h1>
+        <h1 className="text-white text-3xl md:text-6xl md:mt-20 md:mb-20">WELCOME TO FORM AI</h1>
       </div>
       // Demo Video
-      <div className="md:flex md:flex-row md:justify-center md:items-center  ">
-        <div className="border-3 border-white rounded-lg p-3 m-4 md:ml-19 mt-10 mb-2 hover:cursor-pointer hover:bg-orange-500 hover:text-white text-black h-70 md:w-250">
-          <video src=""></video>
+      <div className="flex flex-col items-center justify-center md:flex-row md:justify-center md:items-center  ">
+        <div className="md:flex md:flex-col items-center justify-center">
+          <div className="border-2 border-white rounded-lg p-3 m-4 md:ml-19 mt-10 mb-2   hover:text-white text-black h-70 md:h-90 md:w-150">
+            <video src="/demo-video.mp4" loop muted autoPlay className="fit-cover"></video>
+          </div>
+          <p className="text-gray-300 text-md italic md:text-2xl text-center md:ml-14">Demo Video</p>
         </div>
         // Description Section
-        <div>
+        <div className="mb-30">
           <p className="text-white text-xl m-3 md:text-left">
             Can't afford a personal trainer? No worries, we've got you covered!
             Our AI-powered personal trainer is here to help you achieve your
             fitness goals with personalized workout plans, real-time feedback on
             your form, and progress tracking.
           </p>
-          <Link to="/analyze">
-            <button className="text-white border-2 hover:cursor-pointer bg-[#cfb498] p-3 rounded-3xl mt-4 md:flex">
+          <p className="hidden md:block text-white text-xl m-3 md:text-left">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut explicabo rem dignissimos atque molestias perspiciatis nisi saepe assumenda, minima in, quos distinctio, maiores inventore dolor facere dolorum quia corrupti nesciunt!
+          </p>
+          <div className="flex justify-center items-center md:justify-start focus:none">
+            <Link
+              to="/analyze"
+              className="text-white focus:none border-2 hover:cursor-pointer bg-[#cfb498] p-3 rounded-3xl mt-9 md:flex w-fit"
+            >
               Try Now!
-            </button>
-          </Link>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/components/ui/ExerciseList.jsx
+++ frontend/src/components/ui/ExerciseList.jsx
@@ -2,30 +2,30 @@ function ExerciseList() {
   return (
     <div className="">
       <h2 className="text-white text-3xl m-4 p-4 ">
-        Analyze a wide range exercises
+        Analyze A Wide Range Of Exercises
       </h2>
       <div className="md:flex md:flex-row md:justify-center md:items-center">
-        <div>
-          <div className="border-2 border-white rounded-lg p-3 m-7 mt-1 md:m-4 mb-2 hover:cursor-pointer hover:bg-orange-500 hover:text-white text-white h-70 ">
-            <img src="" alt="person doing push ups" />
+        <div className="flex items-center justify-center ">
+          <div className="border-2 border-white rounded-lg p-3 m-7 mt-1 md:m-4 mb-2 hover:cursor-pointer hover:shadow-lg hover:shadow-gray-500/50 hover:scale-102 hover:text-white text-white h-65 w-75">
+            <div className="h-40 w-full  bg-[url('/pushup-exercise.jpg')] bg-cover bg-center" />
             <p className="text-xl text-left">Push Ups</p>
           </div>
         </div>
-        <div>
-          <div className="border-2 border-white rounded-lg p-3 m-7 mt-10 md:m-4 mb-2 hover:cursor-pointer hover:bg-orange-500 hover:text-white text-white h-70 ">
-            <img src="" alt="person doing squats" />
+        <div className="flex items-center justify-center">
+          <div className="border-2 border-white rounded-lg p-3 m-7 mt-10 md:m-4 mb-2 hover:cursor-pointer hover:shadow-lg hover:shadow-gray-500/50 hover:scale-102 hover:text-white text-white h-65 w-75">
+             <div className="h-40 bg-[url('/squat-exercise.jpg')] bg-cover bg-center" />
             <p className="text-xl text-left">Squats</p>
           </div>
         </div>
-        <div>
-          <div className="border-2 border-white rounded-lg p-3 m-7 mt-10 md:m-4 mb-2 hover:cursor-pointer hover:bg-orange-500 hover:text-white text-white h-70 ">
-            <img src="" alt="person doing bicep curls" />
+        <div className="flex items-center justify-center">
+          <div className="border-2 border-white rounded-lg p-3 m-4 mt-10 md:m-4 mb-2 hover:cursor-pointer hover:shadow-lg hover:shadow-gray-500/50 hover:scale-102 hover:text-white text-white h-65 w-75">
+             <div className="h-40 w-full bg-[url('/bicepCurls-exercise.png')] bg-cover bg-center" />
             <p className="text-xl text-left">Bicep Curls</p>
           </div>
         </div>
-        <div>
-          <div className="border-2 border-white rounded-lg p-3 m-7 mt-10 md:m-4 mb-2 hover:cursor-pointer hover:bg-orange-500 hover:text-white text-white h-70 ">
-            <img src="" alt="person doing shoulder press" />
+        <div className="flex items-center justify-center">
+          <div className="border-2 border-white rounded-lg p-3 m-7 mt-10 md:m-4 mb-2 hover:cursor-pointer hover:shadow-lg hover:shadow-gray-500/50 hover:scale-102 hover:text-white text-white h-65 w-75">
+           <div className="h-40 w-full bg-[url('/shoulderPress-exercise.jpg')] bg-cover bg-center" />
             <p className="text-xl text-left">Shoulder Press</p>
           </div>
         </div>
